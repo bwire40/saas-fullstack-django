a41eebf441e69120d9f549271db48aa64f378cda
bwire40 <emmanuelbwire80@gmail.com>
2025-02-12 19:58:35 +0300

    static files

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my apps
-    'visits'
+    'visits',
+    
 ]
 
 MIDDLEWARE = [
@@ -133,6 +134,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_VENDOR_DIR =STATICFILES_BASE_DIR / 'vendors'
+
+# source for python manage.py collectstatic
+STATICFILES_DIRS=[
+    STATICFILES_BASE_DIR
+]
+
+# output for python manage.py collectstatic
+# local cdn
+STATIC_ROOT = BASE_DIR.parent / "local-cdn"
+
+# production
+# if not DEBUG:
+#     STATIC_ROOT=BASE_DIR / "production"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
